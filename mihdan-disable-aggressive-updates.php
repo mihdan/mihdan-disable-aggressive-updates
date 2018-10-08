85785b14b90e888b713117e83349f4cd8e153168
mihdan <mikhail@kobzarev.com>
2018-10-08 10:48:48 +0300

    Добавил основной код плагина

---
+++ mihdan-disable-aggressive-updates.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name: Mihdan: Disable Aggressive Updates
+ * Description: WordPress плагин для ускорения админки WordPress путём отключения агрессивных проверок обновлений
+ * Version: 1.0
+ * Plugin URI: https://www.kobzarev.com
+ * Author: Mikhail Kobzarev
+ * Author URI: https://www.kobzarev.com
+ * License: GNU General Public License v2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mihdan-disable-aggressive-updates
+ * GitHub Plugin URI: https://github.com/mihdan/mihdan-disable-aggressive-updates
+ * GitHub Branch:     master
+ * Requires WP:       4.9
+ * Requires PHP:      7.2
+ *
+ * @package mihdan-disable-aggressive-updates
+ * @wordpress-plugin
+ * @license   GPL-2.0+
+ * @link https://wp-kama.ru/id_8514/uskoryaem-adminku-wordpress-otklyuchaem-proverki-obnovlenij.html
+ * @see https://wp-kama.ru/filecode/wp-includes/update.php
+ * @author Kama (https://wp-kama.ru)
+ * @version 1.0
+ */
+
+if ( is_admin() ) {
+	// отключим проверку обновлений при любом заходе в админку...
+	remove_action( 'admin_init', '_maybe_update_core' );
+	remove_action( 'admin_init', '_maybe_update_plugins' );
+	remove_action( 'admin_init', '_maybe_update_themes' );
+
+	// отключим проверку обновлений при заходе на специальную страницу в админке...
+	remove_action( 'load-plugins.php', 'wp_update_plugins' );
+	remove_action( 'load-themes.php', 'wp_update_themes' );
+
+	// оставим принудительную проверку при заходе на страницу обновлений...
+	//remove_action( 'load-update-core.php', 'wp_update_plugins' );
+	//remove_action( 'load-update-core.php', 'wp_update_themes' );
+
+	// внутренняя страница админки "Update/Install Plugin" или "Update/Install Theme" - оставим не мешает...
+	//remove_action( 'load-update.php', 'wp_update_plugins' );
+	//remove_action( 'load-update.php', 'wp_update_themes' );
+
+	// событие крона не трогаем, через него будет проверяться наличие обновлений - тут все отлично!
+	//remove_action( 'wp_version_check', 'wp_version_check' );
+	//remove_action( 'wp_update_plugins', 'wp_update_plugins' );
+	//remove_action( 'wp_update_themes', 'wp_update_themes' );
+
+	/**
+	 * отключим проверку необходимости обновить браузер в консоли - мы всегда юзаем топовые браузеры!
+	 * эта проверка происходит раз в неделю...
+	 * @see https://wp-kama.ru/function/wp_check_browser_version
+	 */
+	add_filter( 'pre_site_transient_browser_' . md5( $_SERVER['HTTP_USER_AGENT'] ), '__return_true' );
+}
+
+// eof;
